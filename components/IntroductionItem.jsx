349a3a56ff37d8bb278d998827623a96845aa085
jankat <cankatguven@gmail.com>
2023-12-26 16:12:55 +0300

    bf deploy

--- components/IntroductionItem.jsx
+++ components/IntroductionItem.jsx
@@ -1,18 +1,11 @@
 import Image from "next/image";
 import { useInView } from "react-intersection-observer";
-import React, { useState, useEffect, useRef, useDebugValue } from 'react';
-
+import React, { useState, useEffect, useRef, useDebugValue } from "react";
 
 const IntroductionItem = ({ introImage, title }) => {
+  const [isTouching, setIsTouching] = useState(false);
 
-/*   const [isScrollDown, setIsScrollDown] = useState(false);
-  const [isScrollUp, setIsScrollUp] = useState(false);
-  const [prevScrollPos, setPrevScrollPos] = useState(0);
-  const [elementPosition, setElementPosition] = useState(0);
-  const [onplace, setOnplace] = useState(false);
-  
-  const sectionRef = useRef()
-  const [ref, inView, entry] = useInView({
+  const [ref, inView] = useInView({
     threshold: 0,
     triggerOnce: false,
   });
@@ -20,81 +13,61 @@ const IntroductionItem = ({ introImage, title }) => {
     threshold: 0.1,
     triggerOnce: false,
   });
-  const [ref3, endView] = useInView({
-    threshold: 0,
-    triggerOnce: false,
-  });
 
+  const fixedRef = useRef();
+  const textRef = useRef();
 
-useEffect(() => {
-  console.log(title, ref, inView, entry);
-}, [inView]) */
+  useEffect(() => {
+    const handleScroll = () => {
+      const fixedRect = fixedRef.current.getBoundingClientRect();
+      const otherRect = textRef.current.getBoundingClientRect();
+      const currentIsTouching = !(
+        fixedRect.right < otherRect.left ||
+        fixedRect.left > otherRect.right ||
+        fixedRect.bottom < otherRect.top ||
+        fixedRect.top > otherRect.bottom
+      );
+      setIsTouching(currentIsTouching);
+      console.log(title, "is touching", currentIsTouching);
+    };
+    console.log(title, "STARTTT", startView);
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
 
-const [ref, inView] = useInView({
-  threshold: 0,
-  triggerOnce: false,
-});
-const [ref2, startView] = useInView({
-  threshold: 0.1,
-  triggerOnce: false,
-});
-
-const [isFixed, setIsFixed] = useState(false);
-const photoRef = useRef()
-const textRef = useRef()
-
-useEffect(() => {
-  const handleScroll = () => {
- 
-      const photoRect = photoRef.current.getBoundingClientRect();
-      const textRect = textRef.current.getBoundingClientRect();
-
-      // Fotoğrafın tepesi ekranın en üstündeyken
-      if (photoRect.top >= 0 && photoRect.bottom <= window.innerHeight) {
-        setIsFixed(true);
-      } else if (textRect.top <= 0) {
-        // Yazı ekranın üstünden geçerken ve fotoğrafın altına ulaşırken
-        setIsFixed(false);
-      }
-    
-  };
-
-  window.addEventListener('scroll', handleScroll);
-
-  return () => {
-    window.removeEventListener('scroll', handleScroll);
-  };
-}, []);
-
-
-
-
+  }, []);
 
   return (
-    <section ref={ref} className="md:aspect-square overflow-hidden relative group">
-      <div ref={photoRef}>
-        <div ref={ref2} className="slideController "></div>
-        <div  className="slideControllerBottom z-50 absolute w-1/2 bottom-0"></div>
-        <Image
-          src={introImage}
-          width={900}
-          height={900}
-          alt="plates"
-          className="four-images"
-        />
-        <div className={`w-full absolute  hidden md:block  bottom-8`}>
-          <p className="home-images-title">{title}</p>
-        </div>
-        <div
+    <section
+      
+      className="md:aspect-square overflow-hidden relative group"
+    >
+      <div ref={ref2} className="slideController "></div>
+      <div
         ref={textRef}
-          className={`w-full ${
-            !isFixed && startView
-              ? "fixed"
-              : "absolute"
-          } md:hidden bottom-[32px]`}
-        >
-          <p className="home-images-title ">{title}</p>
-        </div>
+        className="slideControllerBottom z-50 absolute w-full h-0 bottom-8"
+      ></div>
+      <Image
+        src={introImage}
+        width={900}
+        height={900}
+        alt="plates"
+        className="four-images"
+      />
+      <div className={`w-full absolute hidden md:block  bottom-8`}>
+        <p className="home-images-title">{title}</p>
+      </div>
+      <div
+        ref={fixedRef}
+        className={`w-full ${
+          (!isTouching) ? "fixed" : "absolute"
+        } md:hidden bottom-8
+        ${!startView && "hidden" }
+        `}
+        
+      >
+        <p className="home-images-title ">{title}</p>
       </div>
     </section>
   );
@@ -102,8 +75,6 @@ useEffect(() => {
 export default IntroductionItem;
 
 /* object-bottom  md:aspect-square */
-
-
 
 /* const [isScrollDown, setIsScrollDown] = useState(false);
 const [isScrollUp, setIsScrollUp] = useState(false);
@@ -167,4 +138,29 @@ useEffect(() => {
 
 useEffect(() => {
   console.log(title, "element height is: ", sectionRef.current.offsetHeight);
+}, [inView]) */
+
+/*   const [isScrollDown, setIsScrollDown] = useState(false);
+  const [isScrollUp, setIsScrollUp] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [elementPosition, setElementPosition] = useState(0);
+  const [onplace, setOnplace] = useState(false);
+  
+  const sectionRef = useRef()
+  const [ref, inView, entry] = useInView({
+    threshold: 0,
+    triggerOnce: false,
+  });
+  const [ref2, startView] = useInView({
+    threshold: 0.1,
+    triggerOnce: false,
+  });
+  const [ref3, endView] = useInView({
+    threshold: 0,
+    triggerOnce: false,
+  });
+
+
+useEffect(() => {
+  console.log(title, ref, inView, entry);
 }, [inView]) */
