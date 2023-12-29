b5c16b53c164ffd042dc1784d44c27580b3c341b
jankat <cankatguven@gmail.com>
2023-12-29 19:30:03 +0300

    limited edit add

--- components/HomeVitra.jsx
+++ components/HomeVitra.jsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 const HomeVitra = () => {
   return (
     <section className="py-24">
-      <div className="relative overflow-hidden border">
-        <p className="absolute home-images-title bottom-4 sm:bottom-6 md:bottom-8 xl:left-36 xl:top-36 xl:text-6xl">limited edition</p>
+      <div className="relative overflow-hidden">
+        <p className="home-images-title limited-edition">limited edition</p>
         <Image
           src={"/homepage/bsd.jpg"}
           width={2000}
