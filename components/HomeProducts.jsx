c3ba91cc116ab2bda91b3b89e435ecfdb619c3cd
jankat <cankatguven@gmail.com>
2023-12-31 01:37:32 +0300

    animation add

--- components/HomeProducts.jsx
+++ components/HomeProducts.jsx
@@ -5,7 +5,7 @@ import "react-multi-carousel/lib/styles.css";
 
 const HomeProducts = () => {
   return (
-    <section className="">
+    <section className="relative">
       <div className="pb-6 sm:pb-2 max-w-5xl xl:max-w-7xl mx-auto">
         <SectionTitle
           first={"DIRECT AVAILABLE"}
@@ -14,128 +14,130 @@ const HomeProducts = () => {
         />
       </div>
 
-      <Carousel
-      
-        additionalTransfrom={0}
-        arrows={false}
-        centerMode
-        className="max-h-56 sm:max-h-full"
-        dotListClass=""
-        draggable
-        focusOnSelect={false}
-        infinite
-        itemClass=""
-        keyBoardControl
-        minimumTouchDrag={80}
-       
-        pauseOnHover
-        renderArrowsWhenDisabled={false}
-        renderButtonGroupOutside={false}
-        renderDotsOutside={false}
-        responsive={{
-          desktop: {
-            breakpoint: {
-              max: 3000,
-              min: 1024,
+      <div className="">
+        <Carousel
+        
+          additionalTransfrom={0}
+          arrows={false}
+          centerMode
+          className="absolute"
+          dotListClass=""
+          draggable
+          focusOnSelect={false}
+          infinite
+          itemClass=""
+          keyBoardControl
+          minimumTouchDrag={80}
+        
+          pauseOnHover
+          renderArrowsWhenDisabled={false}
+          renderButtonGroupOutside={false}
+          renderDotsOutside={false}
+          responsive={{
+            desktop: {
+              breakpoint: {
+                max: 3000,
+                min: 1024,
+              },
+              items: 3,
+              partialVisibilityGutter: 40,
             },
-            items: 3,
-            partialVisibilityGutter: 40,
-          },
-          mobile: {
-            breakpoint: {
-              max: 464,
-              min: 0,
+            mobile: {
+              breakpoint: {
+                max: 810,
+                min: 0,
+              },
+              items: 1,
+              partialVisibilityGutter: 30,
             },
-            items: 1,
-            partialVisibilityGutter: 30,
-          },
-          tablet: {
-            breakpoint: {
-              max: 1024,
-              min: 464,
+            tablet: {
+              breakpoint: {
+                max: 1024,
+                min: 810,
+              },
+              items: 2,
+              partialVisibilityGutter: 30,
             },
-            items: 2,
-            partialVisibilityGutter: 30,
-          },
-        }}
-        rewind={false}
-        rewindWithAnimation={false}
-        rtl={false}
-        shouldResetAutoplay
-        showDots={false}
-        sliderClass=""
-        slidesToSlide={1}
-        swipeable
-      >
-        <div className=" border-none cursor-pointer flex gap-4">
-          <div className="sm:p-4 ">
-            <Image
-              src={"/plates/p3.png"}
-             
-              quality={100}
-              width={700}
-              height={825}
-              className="rounded-md shadow object-cover home-prod-img"
-              alt="photo"
-            />
-            <div className="homeproducts-info">
-              <p>büyük tabak</p>
-              <p>500tl</p>
+          }}
+          rewind={false}
+          rewindWithAnimation={false}
+          rtl={false}
+          shouldResetAutoplay
+          showDots={false}
+          sliderClass=""
+          slidesToSlide={1}
+          swipeable
+        >
+          <div className=" border-none cursor-pointer flex gap-4">
+            <div className="sm:p-4 ">
+              <Image
+                src={"/plates/p3.png"}
+        
+                quality={100}
+                width={700}
+                height={825}
+                className="rounded-md shadow object-cover home-prod-img h-56 md:h-72"
+                alt="photo"
+              />
+              <div className="homeproducts-info">
+                <p>büyük tabak</p>
+                <p>500tl</p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className=" cursor-pointer">
-          <div className="sm:p-4 ">
-            <Image
-              src={"/plates/p2.png"}
-           
-              quality={100}
-              width={700}
-              height={825}
-              className="rounded-md object-cover shadow home-prod-img"
-              alt="photo"
-            />
-            <div className="homeproducts-info">
-              <p>büyük tabak</p>
-              <p>500tl</p>
+          <div className=" cursor-pointer">
+            <div className="sm:p-4 ">
+              <Image
+                src={"/plates/p2.png"}
+        
+                quality={100}
+                width={700}
+                height={825}
+                className="rounded-md object-cover shadow home-prod-img h-56 md:h-72"
+                alt="photo"
+              />
+              <div className="homeproducts-info">
+                <p>büyük tabak</p>
+                <p>500tl</p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className=" cursor-pointer">
-          <div className="sm:p-4 ">
-            <Image
-              src={"/plates/p4.png"}
-           
-              quality={100}
-              width={700}
-              height={825}
-              className="rounded-md object-cover shadow home-prod-img"
-              alt="photo"
-            />
-            <div className="homeproducts-info">
-              <p>büyük tabak</p>
-              <p>500tl</p>
+          <div className=" cursor-pointer">
+            <div className="sm:p-4 ">
+              <Image
+                src={"/plates/p4.png"}
+        
+                quality={100}
+                width={700}
+                height={825}
+                className="rounded-md object-cover shadow home-prod-img h-56 md:h-72"
+                alt="photo"
+              />
+              <div className="homeproducts-info">
+                <p>büyük tabak</p>
+                <p>500tl</p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className=" cursor-pointer">
-          <div className="sm:p-4 ">
-            <Image
-              src={"/plates/p1.png"}
-            
-              quality={100}
-              width={700}
-              height={825}
-              className="rounded-md object-cover shadow home-prod-img"
-              alt="photo"
-            />
-            <div className="homeproducts-info">
-              <p>büyük tabak</p>
-              <p>500tl</p>
+          <div className=" cursor-pointer">
+            <div className="sm:p-4 ">
+              <Image
+                src={"/plates/p1.png"}
+        
+                quality={100}
+                width={700}
+                height={825}
+                className="rounded-md object-cover shadow home-prod-img h-56 md:h-72"
+                alt="photo"
+              />
+              <div className="homeproducts-info">
+                <p>büyük tabak</p>
+                <p>500tl</p>
+              </div>
             </div>
           </div>
-        </div>
-      </Carousel>
+        </Carousel>
+      </div>
     </section>
   );
 };
