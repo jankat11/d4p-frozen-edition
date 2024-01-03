75df77d502a3cb0efd0174eb9f139fb3e4bcf8cd
jankat <cankatguven@gmail.com>
2024-01-03 21:50:52 +0300

    insta section rf

--- components/home/HomeInstagram.jsx
+++ components/home/HomeInstagram.jsx
@@ -2,6 +2,13 @@ import Image from "next/image";
 import { FaInstagram } from "react-icons/fa";
 import SectionTitle from "../SectionTitle";
 
+const data = [
+  "/homepage/h1.webp",
+  "/homepage/h2.webp",
+  "/homepage/h3.webp",
+  "/homepage/h4.webp",
+];
+
 const HomeInstagram = () => {
   return (
     <section className="pb-8">
@@ -14,54 +21,20 @@ const HomeInstagram = () => {
         />
       </div>
       <div className="grid grid-cols-2 md:grid-cols-4 pt-4 gap-1">
-        <div className="relative group">
-          <div className="absolute top-[50%] transition-all duration-300 right-[50%] cursor-pointer group-hover:opacity-100 z-20 opacity-0 translate-x-[50%] translate-y-[-50%]">
-            <FaInstagram fill="white" size={24} />
+        {data.map((imageItem, i) => (
+          <div key={i} className="relative group">
+            <div className="home-instagram-logo">
+              <FaInstagram fill="white" size={24} />
+            </div>
+            <Image
+              src={imageItem}
+              width={500}
+              height={500}
+              className="home-instagram-img"
+              alt="instagram"
+            />
           </div>
-          <Image
-            src="/homepage/h1.webp"
-            width={500}
-            height={500}
-            className="group-hover:brightness-50 group-hover:cursor-pointer relative z-10 transition-all duration-300 home-prod-img"
-            alt="instagram"
-          />
-        </div>
-        <div className="relative group">
-          <div className="absolute top-[50%] transition-all duration-300 right-[50%] cursor-pointer group-hover:opacity-100 z-20 opacity-0 translate-x-[50%] translate-y-[-50%]">
-            <FaInstagram fill="white" size={24} />
-          </div>
-          <Image
-            src="/homepage/h2.webp"
-            width={500}
-            height={500}
-            className="group-hover:brightness-50 group-hover:cursor-pointer relative z-10 transition-all duration-300 home-prod-img"
-            alt="instagram"
-          />
-        </div>
-        <div className="relative group">
-          <div className="absolute top-[50%] transition-all duration-300 right-[50%] cursor-pointer group-hover:opacity-100 z-20 opacity-0 translate-x-[50%] translate-y-[-50%]">
-            <FaInstagram fill="white" size={24} />
-          </div>
-          <Image
-            src="/homepage/h3.webp"
-            width={500}
-            height={500}
-            className="group-hover:brightness-50 group-hover:cursor-pointer relative z-10 transition-all duration-300 home-prod-img"
-            alt="instagram"
-          />
-        </div>
-        <div className="relative group">
-          <div className="absolute top-[50%] transition-all duration-300 right-[50%] cursor-pointer group-hover:opacity-100 z-20 opacity-0 translate-x-[50%] translate-y-[-50%]">
-            <FaInstagram fill="white" size={24} />
-          </div>
-          <Image
-            src="/homepage/h4.webp"
-            width={500}
-            height={500}
-            className="group-hover:brightness-50 group-hover:cursor-pointer relative z-10 transition-all duration-300 home-prod-img"
-            alt="instagram"
-          />
-        </div>
+        ))}
       </div>
     </section>
   );
