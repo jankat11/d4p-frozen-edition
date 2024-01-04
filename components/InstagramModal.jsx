e1417ce034e97416301134ea2401567fdee7f2c3
jankat <cankatguven@gmail.com>
2024-01-04 13:40:35 +0300

    instagram modal add

--- components/InstagramModal.jsx
+++ components/InstagramModal.jsx
@@ -48,7 +48,7 @@ const InstagramModal = ({ imageItem }) => {
       <div
         ref={overlayRef}
         onClick={handleOverlayClick}
-        className={`bg-black/80 fixed top-0 right-0 mmm left-0 bottom-0 h-full w-full justify-center items-center ${
+        className={`bg-black/80 fixed top-0 right-0 z-50 left-0 bottom-0 h-full w-full justify-center items-center ${
           isModalOpen ? "flex" : "hidden"
         }`}
       >
