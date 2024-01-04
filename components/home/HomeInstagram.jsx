937dc7e547c158ddb47f880aea2da14b5e0acd96
jankat <cankatguven@gmail.com>
2024-01-05 02:25:07 +0300

    instagram modal add

--- components/home/HomeInstagram.jsx
+++ components/home/HomeInstagram.jsx
@@ -3,10 +3,10 @@ import InstagramModal from "../InstagramModal";
 import { FaInstagram } from "react-icons/fa";
 
 const data = [
-  {image: "/homepage/h1.webp", description : "Hayat dönüştürünce güzel! İleri dönüştürülmüş kağıt hamurundan tepsi. Toksik olmayan doğa dostu boya ve cila kullanılarak elde üretilmiştir. Recycling is out, upcycling is in… Upcycled and hand-made papier-maché tray. Coloured and varnished with non-toxic materials.", hash: "#upcycle #papiermache #papiermaché #homedecor #gift #ecofriendly"},
-  {image: "/homepage/h2.webp", description: "Atık kumaşlardan elde üretilmiş yastık kılıfları. Pillow cases made out of waste fabrics.", hash: "#handmade #pillowcase #homedecor #gift #ileridönüşüm #elyapımı #evdekorasyonu #yastık #hediye"},
-  {image: "/homepage/h3.webp", description : "Atık kumaşlardan elde üretilmiş espadril. Hand-made espadrilles made out of waste fabrics.", hash: "#upcycle #ecofriendly #design #handmade #gift #giftideas #crueltyfreegifts #espadrilles #shoes #ileridönüşüm #hediye #dogadostu #espadril #ayakkabı"},
-  {image: "/homepage/h4.webp", description: "Atık kağıtlardan elde ürettiğimiz kase ve çerezlikler toksik olmayan doğa dostu boya ve cilalarla renklendirilmiştir. Papier-maché bowls made out of used newspapers..", hash: " #upcyle #papier-maché #bowl #handmade #ecofriendly #nontoxic #gift #ileridönüşüm #kağıthamuru #kase #hediye"},
+  {image: "/homepage/insta1.png", description : "Hayat dönüştürünce güzel! İleri dönüştürülmüş kağıt hamurundan tepsi. Toksik olmayan doğa dostu boya ve cila kullanılarak elde üretilmiştir. Recycling is out, upcycling is in… Upcycled and hand-made papier-maché tray. Coloured and varnished with non-toxic materials.", hash: "#upcycle #papiermache #papiermaché #homedecor #gift #ecofriendly"},
+  {image: "/homepage/insta2.png", description: "Atık kumaşlardan elde üretilmiş yastık kılıfları. Pillow cases made out of waste fabrics.", hash: "#handmade #pillowcase #homedecor #gift #ileridönüşüm #elyapımı #evdekorasyonu #yastık #hediye"},
+  {image: "/homepage/insta3.png", description : "Atık kumaşlardan elde üretilmiş espadril. Hand-made espadrilles made out of waste fabrics.", hash: "#upcycle #ecofriendly #design #handmade #gift #giftideas #crueltyfreegifts #espadrilles #shoes #ileridönüşüm #hediye #dogadostu #espadril #ayakkabı"},
+  {image: "/homepage/insta4.png", description: "Atık kağıtlardan elde ürettiğimiz kase ve çerezlikler toksik olmayan doğa dostu boya ve cilalarla renklendirilmiştir. Papier-maché bowls made out of used newspapers..", hash: " #upcyle #papier-maché #bowl #handmade #ecofriendly #nontoxic #gift #ileridönüşüm #kağıthamuru #kase #hediye"},
 
 ];
 
