fa0912b96cf6f95cf4c1b33c2e3e674ebd4da878
jankat <cankatguven@gmail.com>
2024-01-05 02:45:06 +0300

    instagram modal add

--- components/InstagramTitle.jsx
+++ components/InstagramTitle.jsx
@@ -5,11 +5,11 @@ const InstagramTitle = ({classes, handleClose}) => {
   return (
     <div className={`mt-0 ml-0 lg:border-b p-3 ${classes}`}>
       <div className="flex items-center justify-between gap-4">
-        <div className="flex items-center gap-4">
+        <div className="">
+          <a href="https://www.instagram.com/design4theplanet" target="_blank" className="flex items-center gap-4">
           <div className="border p-2 rounded-full">
             <FaInstagram fill="black" size={30} />
           </div>
-          <a href="https://www.instagram.com/design4theplanet" target="_blank">
             <p className="text-black tracking-wider font-medium cursor-pointer">
               design4theplanet
             </p>
