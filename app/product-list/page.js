9efa131c5cffc9bee6891a6f7e11e18d04e95ed4
jankat <cankatguven@gmail.com>
2024-01-06 20:17:03 +0300

    aside rm

--- app/product-list/page.js
+++ app/product-list/page.js
@@ -14,25 +14,26 @@ const data = [
 const ProductList = () => {
   return (
     <>
-      <div className="py-24 pb-4 ">
+      <div className="py-24 pb-8">
         <h1 className="text-center text-2xl tracking-wider text-primary">
           All Products
         </h1>
       </div>
 
-      <section className="py-10 pt-0">
-        <div className="mx-auto grid gap-10 max-w-7xl grid-cols-1 p-4 sm:grid-cols-2  lg:grid-cols-3  xl:grid-cols-4">
+      <section className="py-10 pt-0  ">
+        <div className="mx-auto grid gap-10 max-w-7xl grid-cols-1 p-4 pt-0 sm:grid-cols-2  lg:grid-cols-3  xl:grid-cols-4">
           {data.map((product, i) => (
             <article
               key={i}
-              className="relative flex flex-col justify-end border rounded-md shadow-md border-westar lg:first:col-span-2  lg:first:row-span-2  lg:hover:transform cursor-pointer "
+              className="relative flex flex-col justify-end border rounded-md  border-westar/70 lg:first:col-span-2  lg:first:row-span-2  lg:hover:transform cursor-pointer "
             >
               <div className="relative overflow-hidden  flex flex-col justify-center items-center self-center w-full h-full rounded-md">
                 <Image
                   src={product.image}
                   alt="product"
                   width={500}
-                  height={500}
+                  height={600}
+                  style={{width: "500px", height:"auto"}}
                 />
               </div>
 
