c03a8b63ee80c5d41a1ed391232ff25bb24489f2
jankat <cankatguven@gmail.com>
2024-01-08 16:16:06 +0300

    aside rm

--- components/home/CompareImages.jsx
+++ components/home/CompareImages.jsx
@@ -1,5 +1,6 @@
-"use client"
-import ReactCompareImage from "react-compare-image";
+"use client";
+/* import ReactCompareImage from "react-compare-image"; */
+import Image from "next/image";
 import SectionTitle from "../SectionTitle";
 
 const CompareImages = () => {
@@ -14,16 +15,20 @@ const CompareImages = () => {
         />
       </div>
       <div className="flex flex-col sm:flex-row sm:gap-4 sm:px-4  xl:justify-between ">
-        <ReactCompareImage
+        {/* <ReactCompareImage
           leftImage="homepage/comp1.webp"
           rightImage="homepage/comp33.webp"
           sliderPositionPercentage={0.5}
-        />
-        {/*         <ReactCompareImage
-          leftImage="homepage/comp3.png"
-          rightImage="homepage/comp11.png"
-          sliderPositionPercentage={0.6}
         /> */}
+        <div className="diff aspect-[16/9]">
+          <div className="diff-item-1">
+            <Image width={650} height={400} alt="daisy" src="/homepage/comp33.webp" />
+          </div>
+          <div className="diff-item-2">
+            <Image width={650} height={400} alt="daisy" src="/homepage/comp1.webp" />
+          </div>
+          <div className="diff-resizer"></div>
+        </div>
       </div>
     </div>
   );
