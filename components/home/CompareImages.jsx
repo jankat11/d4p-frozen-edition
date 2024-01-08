4a03e04823fb2b21450c34e4e9c5e6def298de34
jankat <cankatguven@gmail.com>
2024-01-08 17:04:08 +0300

    aside rm

--- components/home/CompareImages.jsx
+++ components/home/CompareImages.jsx
@@ -1,8 +1,6 @@
-"use client";
-/* import ReactCompareImage from "react-compare-image"; */
 import Image from "next/image";
 import SectionTitle from "../SectionTitle";
-import { ReactCompareSlider, ReactCompareSliderImage } from 'react-compare-slider';
+import { ReactCompareSlider } from "react-compare-slider";
 
 const CompareImages = () => {
   return (
@@ -16,27 +14,24 @@ const CompareImages = () => {
         />
       </div>
       <div className="flex flex-col sm:flex-row sm:gap-4 sm:px-4  xl:justify-between ">
-        {/* <ReactCompareImage
-          leftImage="homepage/comp1.webp"
-          rightImage="homepage/comp33.webp"
-          sliderPositionPercentage={0.5}
-        /> */}
-{/*         <div className="diff aspect-[16/9]">
-          <div className="diff-item-1">
-            <Image width={650} height={400} alt="daisy" src="/homepage/comp33.webp" />
-          </div>
-          <div className="diff-item-2">
-            <Image width={650} height={400} alt="daisy" src="/homepage/comp1.webp" />
-          </div>
-          <div className="diff-resizer bg-black text-black border-black"></div>
-        </div> */}
-
         <ReactCompareSlider
-      itemOne={<Image width={650} height={400} src="/homepage/comp1.webp" alt="Image one" />}
-      itemTwo={<Image width={650} height={400} src="/homepage/comp33.webp"  alt="Image two" />}
-    />
-
-
+          itemOne={
+            <Image
+              width={650}
+              height={400}
+              src="/homepage/comp1.webp"
+              alt="Image one"
+            />
+          }
+          itemTwo={
+            <Image
+              width={650}
+              height={400}
+              src="/homepage/comp33.webp"
+              alt="Image two"
+            />
+          }
+        />
       </div>
     </div>
   );
